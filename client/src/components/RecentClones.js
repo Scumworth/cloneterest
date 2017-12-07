ab380d43fcc40c10706581db3be32da9bb8b4664
scumworth <danmcninch@gmail.com>
2017-12-06 21:24:15 -0500

    adding user boards

--- client/src/components/RecentClones.js
+++ client/src/components/RecentClones.js
@@ -3,15 +3,30 @@
 import React from 'react';
 import Clone from './Clone';
 import { Modal, Button } from 'react-bootstrap';
+const Loader = require('react-loader');
 
 const RecentClones = ( { recentClonesResults, handleLike, handleReClone,
     handleRemove, user, baseUrl, usersClonesResults, showUser, closeUser,
-    openUser }) => (
+    openUser, usersClonesLoaded }) => (
     <div style = {{ textAlign: 'center' }}>
         <Modal show = { showUser } onHide = { closeUser }>
             <Modal.Header closeButton>
             </Modal.Header>
             <Modal.Body>
+                { usersClonesLoaded && usersClonesResults > 0
+                        ? usersClonesResults.map((result) => <Clone
+                            key = { result.userName + result.title }
+                            userName = { result.userName }
+                            user = { user }
+                            title = { result.title }
+                            imgUrl = { result.imgUrl }
+                            likers = { result.likers }
+                            cloners = { result.cloners }
+                            handleLike = { handleLike }
+                        />)
+                        : null
+                        
+                } 
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick = { closeUser }>Close</Button>
